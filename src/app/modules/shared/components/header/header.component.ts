68aac4305f52a5feebeae53f577cbb47b765fc67
Egor Matsegora <yegor.matsegora@gmail.com>
2020-02-18 16:40:02 +0300

    remove animation on header component inint

--- src/app/modules/shared/components/header/header.component.ts
+++ src/app/modules/shared/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { AuthService } from '../../../core/services/auth/auth.service';
 import { enterAnimation, liveAnimation } from './../../../../animations/header-btn.animation';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, AfterViewInit } from '@angular/core';
 import { trigger, transition, useAnimation } from '@angular/animations';
 import { Router, NavigationStart, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
@@ -17,16 +17,23 @@ import { filter, tap } from 'rxjs/operators';
     ])
   ]
 })
-export class HeaderComponent implements OnDestroy, OnInit {
+export class HeaderComponent implements OnDestroy, OnInit, AfterViewInit {
   private routerEventsSub: Subscription;
   isLoggedIn: boolean = false;
   isAuth: boolean = false;
+  disableAnimation: boolean = true;
 
   constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit() {
     this.changeLinnksVisibility();
     this.setIsLoggedIn();
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.disableAnimation = false;
+    }, 0);
   }
 
   ngOnDestroy() {
