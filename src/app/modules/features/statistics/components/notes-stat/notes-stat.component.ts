7c7857394fe92c697e941d4e1da5862887fcd017
Egor Matsegora <yegor.matsegora@gmail.com>
2020-04-09 14:09:23 +0300

    notes stat styling (not finished)

--- src/app/modules/features/statistics/components/notes-stat/notes-stat.component.ts
+++ src/app/modules/features/statistics/components/notes-stat/notes-stat.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NotesStatistics } from '@app/interfaces/statistics.interface';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'notes-stat',
   templateUrl: './notes-stat.component.html',
@@ -8,10 +10,20 @@ import { NotesStatistics } from '@app/interfaces/statistics.interface';
 })
 export class NotesStatComponent implements OnInit {
   @Input() statistic: NotesStatistics;
+  allNotesNumber: number = 0;
+  todayNotesNumber: number = 0;
 
   constructor() {}
 
   ngOnInit() {
-    console.log(this.statistic);
+    this.setNotesNumber();
+  }
+
+  private setNotesNumber() {
+    this.allNotesNumber = !!this.statistic.allNotesNumber ? this.statistic.allNotesNumber : 0;
+    const today = moment().format('DD.MM.YYYY');
+    this.statistic.notesStat.forEach((stat) => {
+      this.todayNotesNumber = stat.date === today ? stat.notesNumber : 0;
+    });
   }
 }
