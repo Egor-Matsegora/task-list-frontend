940656f214e6e9ade0402abd911dbec133abeee4
Egor Matsegora <yegor.matsegora@gmail.com>
2020-04-17 19:09:54 +0300

    display graph

--- src/app/modules/features/statistics/components/notes-stat/notes-stat.component.ts
+++ src/app/modules/features/statistics/components/notes-stat/notes-stat.component.ts
@@ -1,22 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { NotesStatistics } from '@app/interfaces/statistics.interface';
 
 import * as moment from 'moment';
+import { Chart } from 'chart.js';
+
+import { chartHelper } from './../../helpers/chart.helper';
 
 @Component({
   selector: 'notes-stat',
   templateUrl: './notes-stat.component.html',
   styleUrls: ['./notes-stat.component.scss'],
 })
-export class NotesStatComponent implements OnInit {
+export class NotesStatComponent implements OnInit, AfterViewInit {
   @Input() statistic: NotesStatistics;
   allNotesNumber: number = 0;
   todayNotesNumber: number = 0;
+
+  @ViewChild('graph', { static: true }) graphRef: ElementRef;
 
   constructor() {}
 
   ngOnInit() {
     this.setNotesNumber();
+  }
+
+  ngAfterViewInit() {
+    this.paintGraph();
   }
 
   private setNotesNumber() {
@@ -25,5 +34,17 @@ export class NotesStatComponent implements OnInit {
     this.statistic.notesStat.forEach((stat) => {
       this.todayNotesNumber = stat.date === today ? stat.notesNumber : 0;
     });
+  }
+
+  paintGraph() {
+    const config = {
+      label: 'Заметки',
+      color: '#22b9ff',
+      labels: this.statistic.notesStat.map((item) => item.date),
+      data: this.statistic.notesStat.map((item) => item.notesNumber),
+    };
+    const ctx = this.graphRef.nativeElement.getContext('2d');
+
+    new Chart(ctx, chartHelper(config));
   }
 }
