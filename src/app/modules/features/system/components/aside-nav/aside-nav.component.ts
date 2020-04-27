eaef0ee427c5fa0084c2345a8e00c0ef51c025d2
Egor Matsegora <yegor.matsegora@gmail.com>
2020-04-27 11:13:46 +0300

    add aside user styling

--- src/app/modules/features/system/components/aside-nav/aside-nav.component.ts
+++ src/app/modules/features/system/components/aside-nav/aside-nav.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../../../core/services/auth/auth.service';
 import { AsideStateService } from './../../../../core/services/aside-state/aside-state.service';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
@@ -5,13 +6,13 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'aside-nav',
   templateUrl: './aside-nav.component.html',
-  styleUrls: ['./aside-nav.component.scss']
+  styleUrls: ['./aside-nav.component.scss'],
 })
 export class AsideNavComponent implements OnInit {
   asideState: boolean;
   subscriptions: Subscription = new Subscription();
 
-  constructor(private asideService: AsideStateService) {}
+  constructor(private asideService: AsideStateService, private authService: AuthService) {}
 
   ngOnInit() {
     this.subToAsideState();
@@ -19,9 +20,13 @@ export class AsideNavComponent implements OnInit {
 
   private subToAsideState() {
     this.subscriptions.add(
-      this.asideService.asideState$.subscribe(state => {
+      this.asideService.asideState$.subscribe((state) => {
         this.asideState = state;
       })
     );
+  }
+
+  onExit() {
+    this.authService.logout();
   }
 }
