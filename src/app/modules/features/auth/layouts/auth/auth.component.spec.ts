d5596441a6d9ee6f26f37fe892ce4793f087774e
Egor Matsegora <yegor.matsegora@gmail.com>
2020-05-06 11:09:30 +0300

    add unit tests to auth component

--- src/app/modules/features/auth/layouts/auth/auth.component.spec.ts
+++ src/app/modules/features/auth/layouts/auth/auth.component.spec.ts
@@ -1,25 +1,68 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Location } from '@angular/common';
+import { Routes, Router } from '@angular/router';
 
-// import { AuthComponent } from './auth.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
-// describe('AuthComponent', () => {
-//   let component: AuthComponent;
-//   let fixture: ComponentFixture<AuthComponent>;
+import { AuthComponent } from './auth.component';
+import { LogoComponent } from '@shared/ui-kit/components/logo/logo.component';
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ AuthComponent ]
-//     })
-//     .compileComponents();
-//   }));
+import { Component } from '@angular/core';
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(AuthComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+@Component({
+  template: `login`,
+})
+class LoginComponent {}
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+@Component({
+  template: `registration`,
+})
+class RegistrationComponent {}
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+];
+
+describe('AuthComponent', () => {
+  let component: AuthComponent;
+  let fixture: ComponentFixture<AuthComponent>;
+  let router: Router;
+  let location: Location;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule.withRoutes(routes)],
+      declarations: [AuthComponent, LogoComponent, LoginComponent, RegistrationComponent],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(AuthComponent);
+        component = fixture.componentInstance;
+        router = TestBed.get(Router);
+        location = TestBed.get(Location);
+        router.initialNavigation();
+      });
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should navigate to "login" when path is epty string', fakeAsync(() => {
+    router.navigate(['']);
+    tick();
+    expect(location.path()).toBe('/login', 'inavalid path');
+  }));
+
+  it('should navigate to "registration" when path is registration', fakeAsync(() => {
+    router.navigate(['registration']);
+    tick();
+    expect(location.path()).toBe('/registration', 'inavalid path');
+  }));
+
+  afterEach(() => {
+    fixture.destroy();
+  });
+});
